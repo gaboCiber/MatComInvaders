0a389d87656f2608336be495cd9ec12877417c31
gaboCiber <gabopla.lasa@gmail.com>
2024-07-22 08:17:24 -0400

    Save

--- matcomInvasion.c
+++ matcomInvasion.c
@@ -1,12 +1,27 @@
 #include <ncurses.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
 
-struct player {
+typedef struct  
+{
 	int line, col;
 	char ch;
-};
+} player;
 
-struct player play;
+typedef struct 
+{
+    int line, col;
+	char ch; 
+    int* upDown;
+    int* leftRight;
+    int upDownCount;
+    int leftRightCount; 
+} enemy;
+
+
+player play;
 
 void movePlayer(void *ch)
 {
@@ -28,6 +43,38 @@ void movePlayer(void *ch)
     }
 }
 
+int cases[] = {-1, 0 ,1};
+
+void GetRandomPos(enemy *en)
+{
+    int random = rand();
+    en->col += *(en->leftRight + (rand() % en->leftRightCount));
+    en->line += *(en->upDown + (rand() % en->upDownCount));
+}
+
+void moveEnemy(void *ene)
+{
+    enemy* en = (enemy *) ene;
+    bool insideScreen = 1;
+
+    while (insideScreen)
+    {
+        move(en->line, en->col);
+        addch(' ');
+        
+        GetRandomPos(&en);
+
+        move(en->line, en->col);
+        addch(en->ch);
+
+        insideScreen = en->line > 0 && en->line < LINES && en->col > 0 && en->col < COLS;
+        refresh();
+        sleep(1);
+    }
+    
+}
+
+
 int main() {
     initscr(); // Inicializa ncurses
     keypad(stdscr, TRUE);
@@ -37,23 +84,68 @@ int main() {
     addstr("--MatCOM INVADERS--");
     move (0,1);
     addstr("SCORE: ");
+    
+    refresh();
+
+    // Enemy
+    pthread_t enemyThread;
+    enemy en;
+    en.line = 3;
+    en.col = COLS/2;
+    en.ch = '#';
+    int ar1[] = {-1, 0 , 1};
+    en.leftRight = &ar1;
+    en.leftRightCount = 3;
+    int ar2[] = {0 , 1};
+    en.upDown = &ar2;
+    en.upDownCount = 2;
+
+    move(en.line, en.col);
+    addch(en.ch);
 
     refresh();
 
+    bool insideScreen = 1;
+    while (insideScreen)
+    {
+        move(en.line, en.col);
+        addch(' ');
+        
+        GetRandomPos(&en);
+        GetRandomPos(&en);
+        
+        move(en.line, en.col);
+        addch(en.ch);
+
+        insideScreen = en.line > 0 && en.line < LINES && en.col > 0 && en.col < COLS;
+        sleep(1);
+        refresh();
+    }
+    
+
+    /*int enemyMov = pthread_create(&enemyThread, NULL, moveEnemy, (void*) &en);
+    if(enemyMov != 0)
+    {
+        perror("Error al crear el hilo");
+        return 1;
+    }*/
+
+    // Player
     play.line = LINES-1;
     play.col = COLS/2;
     play.ch = '^';
     
     move(play.line,play.col);
     addch(play.ch);
-    
-    pthread_t playerThreat;
+
+    pthread_t playerThread;    
     int playerMov;
     int ch;
     while ((ch = getch()) != KEY_F(4) && ch != ERR)
     {   
-        playerMov = pthread_create(&playerThreat, NULL, movePlayer, (void *) ch);
+        playerMov = pthread_create(&playerThread, NULL, movePlayer, (void *) ch);
     }    
+    
 
     // Detiene ncurses y restaura el estado del terminal
     endwin();
